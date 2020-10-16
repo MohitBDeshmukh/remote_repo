79e3e052d4bdfbf210960d6765a4bfa85ce5aa81
Mohit Deshmukh <mohit9deshmukh@gmail.com>
2020-10-17 00:29:15 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'hello there';
+        
+        ?>
+        <form name="Form" action="#">
+            <input type="text" name="FirstName" value="" size="20" />
+            <input type="text" name="LastName" value="" size="20" />
+            <input type="radio" name="Gender" value="Female" />
+            <input type="text" name="Email ID" value="" size="20" />
+            <input type="text" name="Contact" value="" size="10" />
+            <input type="radio" name="Gender" value="Male" checked="checked" />
+            <input type="button" value="Submit" name="Submit" />
+        </form>
+    </body>
+</html>
